8ec270eecc8eafd8b195e91427c9075be4c6769f
fatih <meechysm@windowslive.com>
2020-12-15 22:58:35 +0000

    add test

---
+++ tests/Feature/ReportBuilderTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Support\Facades\App;
+use Tests\TestCase;
+
+class ReportBuilderTest extends TestCase
+{
+    /**
+     * @covers ::todo: add class here for coverage
+     */
+    public function testReportBuilderGeneration(): void
+    {
+        // should be something like this
+        // $type = App::make(Sessions::class);
+        // $type->build()->get();
+    }
+}
